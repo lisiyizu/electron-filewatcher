f7c7691ac521350bbbb9a97efa89ee861947fbf7
warnoleto <war.noleto@gmail.com>
2016-08-04 10:41:21 -0300

    adicionando validacao de xml

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,29 @@
 $(function(){
     const fs = require('fs');
+    const xsd = require('xsd-schema-validator');
     
     var watcher = null;
     
-    watchFolder = function(folderpath){
+    watchFolder = function(folderpath, schemapath){
         
-        if (fs.exists(folderpath) && fs.lstatSync(folderpath).isDirectory()){
+        if (fs.statSync(folderpath) && fs.lstatSync(folderpath).isDirectory()){
         
             watcher = fs.watch(folderpath, (event, filename) => {
               if (filename) {
-                fs.exists(folderpath+'\\'+filename, (exists) => {
+                var filepath = folderpath+'\\'+filename;
+                  
+                fs.exists(filepath, (exists) => {
                     if (exists && event === 'rename'){
-                        fs.unlinkSync(folderpath+filename);
-                        $('#textlog').append('<li>processando: '+folderpath+'\\'+filename+'</li>');
+                        var xmlStr = fs.readFileSync(filepath, 'utf8');
+                        xsd.validateXML(xmlStr, schemapath, (err,result) => {
+                            if (!err){
+                                $('#textlog').append('<li>processando: '+filepath+': ' +result.valid+'</li>');
+                            }else{
+                                $('#textlog').append('<li>'+filepath+': ' +err.message+'</li>');
+                            }
+                            fs.unlinkSync(filepath);
+                        });
+                        
                     }
                 });
               }
@@ -27,12 +38,13 @@ $(function(){
 
     $('#watch').click(function(e){
         var folder = $('#folder').val();
+        var schema = $('#schema').val();
 
         if (watcher){
             watcher.close();
         }
         
-        if (watchFolder(folder)){
+        if (watchFolder(folder, schema)){
             $('#textlog').append('<li>monitorando: '+folder+'</li>');
         }
     });
